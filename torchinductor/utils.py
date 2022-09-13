6306d4fd08152aca333938b938d93fd600df14ff
Will Constable <whc@fb.com>
2022-09-13 01:21:27 +0000

    Clean/monitor triton cache during benchmarking

--- torchinductor/utils.py
+++ torchinductor/utils.py
@@ -1,9 +1,13 @@
+import contextlib
 import functools
 import operator
+import os
+import tempfile
 import time
 from typing import Any
 from typing import Dict
 from typing import List
+from unittest import mock
 
 import numpy as np
 import sympy
@@ -221,3 +225,27 @@ def has_incompatible_cudagraph_ops(gm):
         if str(node.target) in forbidden_list:
             return True
     return False
+
+
+class TritonCacheMinder:
+    """
+    Contextmanager used in benchmarking, which provides a clean triton cache dir
+    and also audits the files that were created during a run.
+    """
+    def __init__(self):
+        self.cache_entries = {}
+
+    @contextlib.contextmanager
+    def fresh_cache(self):
+        with tempfile.TemporaryDirectory() as tmpdirname:
+            with mock.patch.dict(os.environ, {"TRITON_CACHE_DIR": tmpdirname}):
+                yield
+                cache_dir = os.path.join(tmpdirname, "cache")
+                files = os.listdir(cache_dir)
+                self.cache_entries = {
+                    f: os.path.getsize(os.path.join(cache_dir, f))
+                    for f in files if ".lock" not in f
+                }
+
+    def get_cache_entries(self):
+        return self.cache_entries
